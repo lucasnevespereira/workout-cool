f192a1f9bded36e663a9295fc8a51c858d9f64b2
lucasnevespereira <pereiraneveslucas@gmail.com>
2025-06-14 15:21:31 +0200

    fix: ci env

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
 import { createEnv } from "@t3-oss/env-nextjs";
 
+const isTest = process.env.NODE_ENV === "test";
+const isCI = process.env.CI === "true";
+
 /**
  * This is the schema for the environment variables.
  *
@@ -30,4 +33,5 @@ export const env = createEnv({
     NEXT_PUBLIC_OPENPANEL_CLIENT_ID: process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID,
     NEXT_PUBLIC_FACEBOOK_PIXEL_ID: process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID,
   },
+  skipValidation: isTest || isCI,
 });
